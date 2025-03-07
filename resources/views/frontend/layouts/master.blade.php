01f2df0c22780cc45643e36e4e80c592bb7ef166
JOJO <a78751233@gmail.com>
2025-03-08 03:36:52 +0800

    Prepare for deployment

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="zh-Hant-TW">
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	@include('frontend.layouts.head')	
 </head>
 <body class="bg-primary">
