2e54e5c6885282ecd5c6d0b958410d732447cdfc
JOJO <a78751233@gmail.com>
2025-03-07 19:09:32 +0800

    Prepare for deployment

--- worker.sh
+++
@@ -1,6 +0,0 @@
-composer require predis/predis
-npm install --save socket.io-client
-npm install --save laravel-echo
-npm install -g laravel-echo-server
-php artisan queue:work &
-laravel-echo-server start &
