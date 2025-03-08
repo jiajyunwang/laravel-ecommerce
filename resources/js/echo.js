96b6391218d5ba3138ac3097159ee2179e4454f2
JOJO <a78751233@gmail.com>
2025-03-09 03:57:07 +0800

    Prepare for deployment

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -18,5 +18,5 @@ import io from 'socket.io-client';
 window.io = io;
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: 'laravel-echo-server.zeabur.app'
+    host: window.location.hostname + ':6001'
 });
