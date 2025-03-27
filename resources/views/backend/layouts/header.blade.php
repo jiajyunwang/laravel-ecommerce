7e60ccfa5e6d65b5df3b5bcdaf3002598297cdff
JOJO <a78751233@gmail.com>
2025-03-27 21:44:03 +0800

    Prepare for deployment

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -8,9 +8,9 @@
 <div class="topbar">
     <div class="row">
         <a href="/admin/product"><div class="{{ $productCol }}"><p>商品(<p>{{Helper::getProductCount('active')}}</p>)</p></div></a>
-        <a href="/admin/product?type=unlisted"><div class="{{ $unlistedCol }}"><p>已下架(<p>{{Helper::getProductCount('inactive')}}</p>)</p></div></a>
-        <a href="/admin/order?type=unhandled"><div class="{{ $unhandledCol }}"><p>待出貨(<p class="count">{{Helper::getAdminOrdertCount('unhandled')}}</p>)</p></div></a>
-        <a href="/admin/order?type=shipping"><div class="{{ $shippingCol }}"><p>待收貨(<p class="count">{{Helper::getAdminOrdertCount('shipping')}}</p>)</p></div></a>
+        <a href="/admin/product?type=unlisted"><div class="{{ $unlistedCol }}"><p>已下架(<span>{{Helper::getProductCount('inactive')}}</span>)</p></div></a>
+        <a href="/admin/order?type=unhandled"><div class="{{ $unhandledCol }}"><p>待出貨(<span class="count">{{Helper::getAdminOrdertCount('unhandled')}}</span>)</p></div></a>
+        <a href="/admin/order?type=shipping"><div class="{{ $shippingCol }}"><p>待收貨(<span class="count">{{Helper::getAdminOrdertCount('shipping')}}</span>)</p></div></a>
         <a href="/admin/order?type=completed"><div class="{{ $completedCol }}"><p>已完成</p></div></a>
         <a href="/admin/order?type=cancel"><div class="{{ $cancelCol }}"><p>已取消</p></div></a>
     </div>
