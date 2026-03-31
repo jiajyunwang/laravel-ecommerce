e9a1b787da2be5387350e4639c2e9f35d33cdbb2
JOJO <a78751233@gmail.com>
2026-03-31 17:06:53 +0800

    refactor: rewrite cart using Vue

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProductIndex from '@/components/ProductIndex.vue'
+import ProductDetail from '@/components/ProductDetail.vue'
 import Register from '@/components/Register.vue'
 import Login from '@/components/Login.vue'
 import Account from '@/components/Account.vue'
-import ProductDetail from '@/components/ProductDetail.vue'
+import Cart from '@/components/Cart.vue'
 
 const routes = [
   { path: '/', component: ProductIndex },
@@ -11,6 +12,7 @@ const routes = [
   { path: '/register', component: Register },
   { path: '/login', component: Login },
   { path: '/account', component: Account },
+  { path: '/cart', component: Cart },
 ]
 
 const router = createRouter({
